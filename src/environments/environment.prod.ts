86c432c532ac03e4cc96a384a2dd8e23ca351523
mbenanes <malek.benanes@gmail.com>
2020-02-01 21:00:47 +0100

    change production server port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://laplume-dor.tn:8091/',
-  BASE_API_URL: 'http://laplume-dor.tn:8091/api/'
+  API_URL: 'http://laplume-dor.tn/',
+  BASE_API_URL: 'http://laplume-dor.tn/api/'
 };
