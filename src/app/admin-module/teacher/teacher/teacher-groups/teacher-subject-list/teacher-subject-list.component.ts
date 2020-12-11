5abcb53a1b9c6f11548cf298a2a95da146e8f1c9
Omar-86 <benanesmalek@gmail.com>
2020-12-11 22:53:34 +0100

    little refactor

--- src/app/admin-module/teacher/teacher/teacher-groups/teacher-subject-list/teacher-subject-list.component.ts
+++ src/app/admin-module/teacher/teacher/teacher-groups/teacher-subject-list/teacher-subject-list.component.ts
@@ -1,29 +1,29 @@
 import * as _ from 'lodash';
-import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
-import { SubjectService } from '@app/services/subject.service';
-import { Group } from '@app/models/Group';
-import { Subject } from '@app/models/Subject';
-import { LessonService } from '@app/services/lesson.service';
-import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
-import { Lesson } from '@app/models/Lesson';
-import { Teacher } from '@app/models/Teacher.model';
-import { TranslateService } from '@ngx-translate/core';
+import {Component, OnInit, Input, SimpleChanges, OnChanges} from '@angular/core';
+import {SubjectService} from '@app/services/subject.service';
+import {Group} from '@app/models/Group';
+import {Subject} from '@app/models/Subject';
+import {LessonService} from '@app/services/lesson.service';
+import {FormBuilder, FormGroup, FormArray, FormControl} from '@angular/forms';
+import {Lesson} from '@app/models/Lesson';
+import {Teacher} from '@app/models/Teacher.model';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-teacher-subject-list',
   templateUrl: './teacher-subject-list.component.html',
-  styleUrls: ['./teacher-subject-list.component.scss']
+  styleUrls: ['./teacher-subject-list.component.scss'],
 })
 export class TeacherSubjectListComponent implements OnInit, OnChanges {
-  // URL dynamic fields form : https://alligator.io/angular/reactive-forms-formarray-dynamic-fields/
-  // http://marcusresell.com/2018/07/18/dynamic-checkbox-angular/
+  @Input('selectedGroup')
+  selectedGroup: Group;
 
-  @Input('selectedGroup') selectedGroup: Group;
-  @Input('lessons') lessons: Lesson[];
-  @Input('teacher') teacher: Teacher;
+  @Input('teacher')
+  teacher: Teacher;
 
   subjects: Subject[];
 
+  lessons: Lesson[];
   subjectsPerLevel: Subject[];
 
   subjectsForm: FormGroup;
@@ -47,26 +47,30 @@ export class TeacherSubjectListComponent implements OnInit, OnChanges {
   private constructView(selectedGroup: Group) {
     this.initForm();
     this.subjectService.findAll()
-                       .subscribe(subjects => {
-                         this.subjects = subjects;
-                         this.subjectsPerLevel = this.subjectService.filter( this.subjects, selectedGroup.level);
-                         this.updateForm();
-                        }, err => console.log('check error: ', err));
+        .subscribe((subjects) => {
+          this.subjects = subjects;
+          this.subjectsPerLevel = this.subjectService.filter( this.subjects, selectedGroup.level);
+          this.updateForm();
+        }, (err) => console.log('check error: ', err));
   }
 
   submit() {
     const checkedOptions = this.subjectsForm.value.checkedOptions
-      .map((checked, index) => checked ? { id: this.checkedOptions[index].id } : null)
-      .filter(value => value !== null);
+        .map((checked, index) => checked ? {id: this.checkedOptions[index].id} : null)
+        .filter((value) => value !== null);
     const uncheckedOptions = _.differenceBy(this.subjectsPerLevel, checkedOptions, 'id');
-    _.forEach(checkedOptions, checkedOption => { this.save(this.teacher, this.findSubject(checkedOption.id), this.selectedGroup )});
-    _.forEach(uncheckedOptions, uncheckedOption => { this.delete(this.teacher, this.findSubject(uncheckedOption.id), this.selectedGroup )});
+    _.forEach(checkedOptions, (checkedOption) => {
+      this.save(this.teacher, this.findSubject(checkedOption.id), this.selectedGroup );
+    });
+    _.forEach(uncheckedOptions, (uncheckedOption) => {
+      this.delete(this.teacher, this.findSubject(uncheckedOption.id), this.selectedGroup );
+    });
   }
 
   private initForm() {
-    const formControls = this.checkedOptions.map(control => new FormControl(false));
+    const formControls = this.checkedOptions.map((control) => new FormControl(false));
     this.subjectsForm = this.fb.group({
-      checkedOptions: new FormArray(formControls)
+      checkedOptions: new FormArray(formControls),
     });
   }
 
@@ -77,7 +81,7 @@ export class TeacherSubjectListComponent implements OnInit, OnChanges {
   }
 
   private createCheckBoxes() {
-    return _.map(this.checkedOptions, subject => this.createCheckBox(subject.id, this.selectedGroup.id))
+    return _.map(this.checkedOptions, (subject) => this.createCheckBox(subject.id, this.selectedGroup.id));
   }
 
   private createCheckBox(subjectId: Subject, groupId: string) {
@@ -88,7 +92,9 @@ export class TeacherSubjectListComponent implements OnInit, OnChanges {
   }
 
   private updateSubjects(subjects: Subject[]) {
-    return _.map(subjects, subject => {return {id: subject.id, name: subject.name};});
+    return _.map(subjects, (subject) => {
+      return {id: subject.id, name: subject.name};
+    });
   }
 
   private save(teacher: Teacher, subject: Subject, group: Group) {
@@ -98,11 +104,11 @@ export class TeacherSubjectListComponent implements OnInit, OnChanges {
 
   delete(teacher: Teacher, subject: Subject, group: Group) {
     const lesson = this.createNewLesson(teacher, subject, group);
-    this.lessonService.delete(lesson).subscribe(/*(lessn: Lesson) => this.lessons.slice(this.find)*/);
+    this.lessonService.delete(lesson).subscribe(/* (lessn: Lesson) => this.lessons.slice(this.find)*/);
   }
 
   createNewLesson(teacher: Teacher, subject: Subject, group: Group) {
-   const lesson = new Lesson();
+    const lesson = new Lesson();
     lesson.id.teacherId = teacher.id;
     lesson.id.subjectId = subject.id;
     lesson.id.groupId = group.id;
@@ -110,10 +116,10 @@ export class TeacherSubjectListComponent implements OnInit, OnChanges {
     lesson.groupName = group.name;
     lesson.teacherName = teacher.firstName;
     lesson.subjectName = subject.name;
-    return  lesson;
+    return lesson;
   }
 
   private findSubject(subjectId: string): Subject {
-    return _.find(this.subjects, {id: subjectId})
+    return _.find(this.subjects, {id: subjectId});
   }
 }
