c9eb7ed7491fae63218108173a71dd021617f99b
Omar-86 <benanesmalek@gmail.com>
2020-12-21 19:55:35 +0100

    fix group list

--- src/app/admin-module/group/group-list/group-list.component.ts
+++ src/app/admin-module/group/group-list/group-list.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Group } from '@app/models/Group.model';
-import { Level } from '@app/models/enums/Level';
-import { GroupService } from '@app/services/group.service';
-import { Routers } from '@app/admin-module/routes/router-link';
-import { TranslateService } from '@ngx-translate/core';
+import {Component, OnInit} from '@angular/core';
+import {Group} from '@app/models/Group.model';
+import {Level} from '@app/models/enums/Level';
+import {GroupService} from '@app/services/group.service';
+import {Routers} from '@app/admin-module/routes/router-link';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-group-list',
   templateUrl: './group-list.component.html',
-  styleUrls: ['./group-list.component.scss']
+  styleUrls: ['./group-list.component.scss'],
 })
 export class GroupListComponent implements OnInit {
-
   GROUP_DETAIL: string = Routers.APP_GROUP_DETAIL;
 
   groups: Group[];
@@ -24,7 +23,6 @@ export class GroupListComponent implements OnInit {
   }
 
   private findAll(): void {
-    this.groupService.findAll().subscribe(groups => this.groups = groups, err => console.log(err));
+    this.groupService.findAll().subscribe((groups) => this.groups = groups, (err) => console.log(err));
   }
-
 }
