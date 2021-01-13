c5ca8576b15a895d86b5f90cedeb97faa49a72a3
fakhrovski <fakher.hannafi@gmail.com>
2021-01-13 23:29:57 +0100

    Update env endpoints
    Add prod domain names

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://ec2-54-228-174-167.eu-west-1.compute.amazonaws.com:8091/',
-  baseApiEndpoint: 'http://ec2-54-228-174-167.eu-west-1.compute.amazonaws.com:8091/api/',
-  resourceEndpoint: 'http://www.laplume-dor.tn/uploads/'
+  apiEndpoint: 'https://bck.laplumedor.net:8091/',
+  baseApiEndpoint: 'https://bck.laplumedor.net:8091/api/',
+  resourceEndpoint: 'https://www.laplumedor.net/uploads/'
 };
